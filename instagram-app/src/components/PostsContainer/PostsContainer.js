5dc904ec88ca8c1c5ccf8857084ed88d80cc4085
Matt Conway <mattconway727@gmail.com>
2019-05-25 14:08:22 -0400

    fixed PostsContainer.js

--- instagram-app/src/components/PostsContainer/PostsContainer.js
+++ instagram-app/src/components/PostsContainer/PostsContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 
-const PostContainer = props => {
+const PostsContainer = props => {
   return (
     <div className="posts-container-wrapper">
       {props.posts.map(p => (
