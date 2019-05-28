ee80d98de6515ec603dab531c676abf52dc05141
Matt Conway <mattconway727@gmail.com>
2019-05-28 19:27:42 -0400

    updated App.js

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import dummyData from "./dummy-data";
 import PostsContainer from "./components/PostsContainer/PostsContainer";
 import SearchBar from "./components/SearchBar/SearchBarContainer";
 
-class App extends React.Component {
+class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData
+      posts: [],
+      filteredPosts: []
     };
   }
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+  searchPostsHandler = e => {
+    const posts = this.state.posts.filter(p => {
+      if (p.username.includes(e.target.value)) {
+        return p;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <PostsContainer posts={this.state.posts} />
+        <SearchBar searchPosts={this.searchPostsHandler} />
+        <PostsContainer
+          posts={
+            this.state.filteredPosts.length > 0
+              ? this.state.filteredPosts
+              : this.state.posts
+          }
+        />
       </div>
     );
   }
