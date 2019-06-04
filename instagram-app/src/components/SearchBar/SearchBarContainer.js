221b8cd8fbfe739fef6ae1e41eab76288590f97d
Matt Conway <mattconway727@gmail.com>
2019-06-03 22:08:09 -0400

    All code complete, just need to add dependencies

--- instagram-app/src/components/SearchBar/SearchBarContainer.js
+++ instagram-app/src/components/SearchBar/SearchBarContainer.js
@@ -1,32 +1,95 @@
 import React from "react";
+import CameraLogo from "../../assets/igcameralogo.png";
+import IGLogo from "../../assets/iglogo.png";
+import styled, { css } from "styled-components";
 
-import "./SearchBar.css";
+const Header = styled.header`
+  border-bottom: 1px solid #3333;
+  height: 80px;
+  line-height: 70px;
+  display: flex;
+  justify-content: space-between;
+  background: #ffffff;
+`;
 
-const SearchBar = () => {
+const LogoHeader = styled.div`
+  width: 300px;
+  line-height: 75px;
+`;
+
+const LogoImage = styled.img`
+  height: 50%;
+  padding: 0px 15px;
+  @media (max-width: 785px) {
+    display: none;
+  }
+`;
+
+const LogoCameraImage = styled.img`
+  height: 50%;
+  padding: 0px 15px;
+`;
+
+const Divider = styled.span`
+  width: 50px;
+  @media (max-width: 785px) {
+    display: none;
+  }
+`;
+
+const InputWrapper = styled.div`
+  width: 300px;
+  margin: auto;
+  display: flex;
+`;
+
+const SearchInput = styled.input`
+  height: 30px;
+  width: 300px;
+  text-align: center;
+`;
+
+const IconsWrapper = styled.div`
+  display: flex;
+  width: 300px;
+`;
+const SearchIcon = styled.i`
+  position: absolute;
+  display: inline;
+  top: 33px;
+  padding-left: 10px;
+`;
+const SocialDiv = styled.div`
+  width: 50px;
+`;
+const SearchBar = props => {
   return (
-    <div className="search-bar-wrapper">
-      <div className="image-wrapper">
-        <img
-          alt="instagram logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
-          className="logo-image"
+    <Header>
+      <LogoHeader>
+        <LogoCameraImage alt="instagram camera" src={CameraLogo} />
+        <Divider>|</Divider>
+        <LogoImage alt="instagram logo" src={IGLogo} />
+      </LogoHeader>
+      <InputWrapper>
+        <SearchIcon className="fas fa-search" />
+        <SearchInput
+          type="text"
+          placeholder="Search"
+          onKeyDown={props.searchPosts}
         />
-      </div>
-      <div>
-        <input type="text" placeholder="Search" />
-      </div>
-      <div className="social-wrapper">
-        <div className="social">
-          <i className="fa fa-compass" />
-        </div>
-        <div className="social">
-          <i className="fa fa-heart" />
-        </div>
-        <div className="social">
-          <i className="fa fa-user-circle" />
-        </div>
-      </div>
-    </div>
+      </InputWrapper>
+      <IconsWrapper>
+        <SocialDiv>
+          <i className="far fa-compass" />
+        </SocialDiv>
+        <SocialDiv>
+          <i className="far fa-heart" />
+        </SocialDiv>
+        <SocialDiv>
+          <i className="far fa-user-circle" />
+        </SocialDiv>
+      </IconsWrapper>
+    </Header>
   );
 };
 
